061bae664052389cd43c2b5d38aa366569b87294
olegratii <olegratii@gmail.com>
2017-11-06 14:54:16 +0200

    initial site version

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+   $(".section-label").click(function(){
+	   $(this).siblings(".section-wrapper").toggleClass("hide-block");
+	   $(this).siblings(".section-wrapper").toggleClass("show-block");
+   })
+
+   $(".image-label").click(function(){
+	   $(this).siblings(".asset-img").toggleClass("hide-block");
+	   $(this).siblings(".asset-img").toggleClass("show-block");
+   })
+
+});
